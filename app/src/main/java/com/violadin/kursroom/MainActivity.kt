74913363e51c77a1c33a778d7dc82734590b5f9e
VioladiN <violadin.kisel@mail.ru>
2020-12-01 18:55:21 +0300

    Upload project

---
+++ app/src/main/java/com/violadin/kursroom/MainActivity.kt
@@ -0,0 +1,11 @@
+ package com.violadin.kursroom
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
